1d19958e77b954ac6831597fce33ebe9552fd06e
SebRol <8366431+SebRol@users.noreply.github.com>
2019-05-22 11:56:08 +0200

    initial

---
+++ perf/Test/TestDataAccelerometer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using Android.Hardware;
+using Android.Runtime;
+using Xamarin.Forms;
+
+namespace perf
+{
+   public class TestDataAccelerometer
+   {
+      static TestData testData = new TestDataAccelerometer01();
+      static int index;
+      static int counter;
+      static bool enabled;
+
+      public static bool IsEnabled() 
+      { 
+         counter++;
+
+         if (counter >= testData.Data.Length)
+         {
+            enabled = !enabled;
+            counter = 0;
+         }
+
+         return enabled;
+      }
+
+      public static void Advance() 
+      { 
+         index++;
+
+         if (index >= testData.Data.Length)
+         {
+            index = 0;
+         }
+      }
+
+      public static long  Timestamp { get { return (long)(testData.Data[index].Time * 1000000000); } }
+      public static float X         { get { return testData.Data[index].X; } }
+      public static float Y         { get { return testData.Data[index].Y; } }
+      public static float Z         { get { return testData.Data[index].Z; } }
+      public static float Speed     { get { return testData.Data[index].Speed; } }
+      public static float PeakA     { get { return testData.Data[index].PeakA; } }
+   }
+}
---
+++ perf/Test/TestDataType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace perf
+{
+   public interface TestData
+   {
+      TestDataType[] Data { get; }
+   }
+
+   public class TestDataType
+   {
+      public float Time;
+      public float X;
+      public float Y;
+      public float Z;
+      public float Speed;
+      public float PeakA;
+
+      public TestDataType(float t, float x, float y, float z, float s, float p)
+      {
+         this.Time  = t;
+         this.X     = x;
+         this.Y     = y;
+         this.Z     = z;
+         this.Speed = s;
+         this.PeakA = p;
+      }
+
+      public TestDataType(double t, double x, double y, double z, double s, double p)
+      {
+         this.Time  = (float)t;
+         this.X     = (float)x;
+         this.Y     = (float)y;
+         this.Z     = (float)z;
+         this.Speed = (float)s;
+         this.PeakA = (float)p;
+      }
+   }
+}
