410c83df9628fed4e311ae9590ff1fed279db132
SebRol <8366431+SebRol@users.noreply.github.com>
2019-05-22 11:40:41 +0200

    initial

---
+++ Droid/Resources/OxyPlotBuildFix.cs
@@ -0,0 +1,219 @@
+﻿
+namespace perf.Droid
+{
+   public class OxyPlotBuildFix
+   {
+      public OxyPlotBuildFix()
+      {
+      }
+   }
+
+   public partial class Resource
+   {
+      public partial class String
+      {
+         public static int ApplicationName;
+         public static int Hello;
+      }
+   }
+
+#if false
+   public partial class Resource
+   {
+      public partial class Attribute
+      {
+         public static int mediaRouteSettingsDrawable;
+      }
+
+      public partial class Color
+      {
+         public static int design_textinput_error_color;
+      }
+
+      public partial class Dimension
+      {
+         public static int design_fab_content_size;
+         public static int design_navigation_padding_top_default;
+         public static int design_tab_min_width;
+         public static int dialog_fixed_height_major;
+         public static int dialog_fixed_height_minor;
+         public static int dialog_fixed_width_major;
+         public static int dialog_fixed_width_minor;
+         public static int mr_media_route_controller_art_max_height;
+      }
+      
+      public partial class Drawable
+      {
+         public static int ic_setting_dark;
+         public static int ic_setting_light;
+         public static int mr_ic_settings_dark;
+         public static int mr_ic_settings_light;
+      }
+
+      public partial class Id
+      {
+         public static int art;
+         public static int buttons;
+         public static int default_control_frame;
+         public static int disconnect;
+         public static int media_route_control_frame;
+         public static int media_route_list;
+         public static int media_route_volume_layout;
+         public static int media_route_volume_slider;
+         public static int play_pause;
+         public static int route_name;
+         public static int settings;
+         public static int stop;
+         public static int subtitle;
+         public static int title_bar;
+      }
+      
+      public partial class Layout
+      {
+         public static int mr_media_route_chooser_dialog;
+         public static int mr_media_route_controller_material_dialog_b;
+         public static int mr_media_route_list_item;
+         public static int mr_media_route_button_content_description;
+         public static int mr_media_route_chooser_searching;
+         public static int mr_media_route_chooser_title;
+         public static int mr_media_route_controller_disconnect;
+         public static int mr_media_route_controller_no_info_available;
+         public static int mr_media_route_controller_pause;
+         public static int mr_media_route_controller_play;
+      }
+
+      public partial class String
+      {
+         public static int mr_media_route_controller_settings_description;
+         public static int mr_media_route_controller_stop;
+         public static int mr_media_route_list_item;
+         public static int mr_media_route_button_content_description;
+         public static int mr_media_route_chooser_searching;
+         public static int mr_media_route_chooser_title;
+         public static int mr_media_route_controller_disconnect;
+         public static int mr_media_route_controller_no_info_available;
+         public static int mr_media_route_controller_pause;
+         public static int mr_media_route_controller_play;
+      }
+         
+      public partial class Style
+      {
+         public static int RtlOverlay_Widget_AppCompat_ActionButton_Overflow;
+      }
+
+      public partial class Styleable
+      {
+         public static int FloatingActionButton_android_background;
+         public static int[] Theme;
+         public static int Theme_actionBarDivider;
+         public static int Theme_actionBarItemBackground;
+         public static int Theme_actionBarPopupTheme;
+         public static int Theme_actionBarSize;
+         public static int Theme_actionBarSplitStyle;
+         public static int Theme_actionBarStyle;
+         public static int Theme_actionBarTabBarStyle;
+         public static int Theme_actionBarTabStyle;
+         public static int Theme_actionBarTabTextStyle;
+         public static int Theme_actionBarTheme;
+         public static int Theme_actionBarWidgetTheme;
+         public static int Theme_actionButtonStyle;
+         public static int Theme_actionDropDownStyle;
+         public static int Theme_actionMenuTextAppearance;
+         public static int Theme_actionMenuTextColor;
+         public static int Theme_actionModeBackground;
+         public static int Theme_actionModeCloseButtonStyle;
+         public static int Theme_actionModeCloseDrawable;
+         public static int Theme_actionModeCopyDrawable;
+         public static int Theme_actionModeCutDrawable;
+         public static int Theme_actionModeFindDrawable;
+         public static int Theme_actionModePasteDrawable;
+         public static int Theme_actionModePopupWindowStyle;
+         public static int Theme_actionModeSelectAllDrawable;
+         public static int Theme_actionModeShareDrawable;
+         public static int Theme_actionModeSplitBackground;
+         public static int Theme_actionModeStyle;
+         public static int Theme_actionModeWebSearchDrawable;
+         public static int Theme_actionOverflowButtonStyle;
+         public static int Theme_actionOverflowMenuStyle;
+         public static int Theme_activityChooserViewStyle;
+         public static int Theme_alertDialogButtonGroupStyle;
+         public static int Theme_alertDialogCenterButtons;
+         public static int Theme_alertDialogStyle;
+         public static int Theme_alertDialogTheme;
+         public static int Theme_android_windowAnimationStyle;
+         public static int Theme_android_windowIsFloating;
+         public static int Theme_autoCompleteTextViewStyle;
+         public static int Theme_borderlessButtonStyle;
+         public static int Theme_buttonBarButtonStyle;
+         public static int Theme_buttonBarNegativeButtonStyle;
+         public static int Theme_buttonBarNeutralButtonStyle;
+         public static int Theme_buttonBarPositiveButtonStyle;
+         public static int Theme_buttonBarStyle;
+         public static int Theme_buttonStyle;
+         public static int Theme_buttonStyleSmall;
+         public static int Theme_checkboxStyle;
+         public static int Theme_checkedTextViewStyle;
+         public static int Theme_colorAccent;
+         public static int Theme_colorButtonNormal;
+         public static int Theme_colorControlActivated;
+         public static int Theme_colorControlHighlight;
+         public static int Theme_colorControlNormal;
+         public static int Theme_colorPrimary;
+         public static int Theme_colorPrimaryDark;
+         public static int Theme_colorSwitchThumbNormal;
+         public static int Theme_controlBackground;
+         public static int Theme_dialogPreferredPadding;
+         public static int Theme_dialogTheme;
+         public static int Theme_dividerHorizontal;
+         public static int Theme_dividerVertical;
+         public static int Theme_dropDownListViewStyle;
+         public static int Theme_dropdownListPreferredItemHeight;
+         public static int Theme_editTextBackground;
+         public static int Theme_editTextColor;
+         public static int Theme_editTextStyle;
+         public static int Theme_homeAsUpIndicator;
+         public static int Theme_listChoiceBackgroundIndicator;
+         public static int Theme_listDividerAlertDialog;
+         public static int Theme_listPopupWindowStyle;
+         public static int Theme_listPreferredItemHeight;
+         public static int Theme_listPreferredItemHeightLarge;
+         public static int Theme_listPreferredItemHeightSmall;
+         public static int Theme_listPreferredItemPaddingLeft;
+         public static int Theme_listPreferredItemPaddingRight;
+         public static int Theme_panelBackground;
+         public static int Theme_panelMenuListTheme;
+         public static int Theme_panelMenuListWidth;
+         public static int Theme_popupMenuStyle;
+         public static int Theme_popupWindowStyle;
+         public static int Theme_radioButtonStyle;
+         public static int Theme_ratingBarStyle;
+         public static int Theme_searchViewStyle;
+         public static int Theme_selectableItemBackground;
+         public static int Theme_selectableItemBackgroundBorderless;
+         public static int Theme_spinnerDropDownItemStyle;
+         public static int Theme_spinnerStyle;
+         public static int Theme_switchStyle;
+         public static int Theme_textAppearanceLargePopupMenu;
+         public static int Theme_textAppearanceListItem;
+         public static int Theme_textAppearanceListItemSmall;
+         public static int Theme_textAppearanceSearchResultSubtitle;
+         public static int Theme_textAppearanceSearchResultTitle;
+         public static int Theme_textAppearanceSmallPopupMenu;
+         public static int Theme_textColorAlertDialogListItem;
+         public static int Theme_textColorSearchUrl;
+         public static int Theme_toolbarNavigationButtonStyle;
+         public static int Theme_toolbarStyle;
+         public static int Theme_windowActionBar;
+         public static int Theme_windowActionBarOverlay;
+         public static int Theme_windowActionModeOverlay;
+         public static int Theme_windowFixedHeightMajor;
+         public static int Theme_windowFixedHeightMinor;
+         public static int Theme_windowFixedWidthMajor;
+         public static int Theme_windowFixedWidthMinor;
+         public static int Theme_windowMinWidthMajor;
+         public static int Theme_windowMinWidthMinor;
+         public static int Theme_windowNoTitle;
+      }
+   }
+#endif
+}
